3f32c6e99045d60a8afc8ff74bccdd2043036162
Julia Friesel <julia.friesel@gmail.com>
2017-01-18 16:45:55 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+function mkdir(path) {
+    return new Promise((resolve, reject) => {
+        fs.mkdir(path, (err) => {
+            if (err) reject(err);
+            resolve();
+        });
+    });
+}
+
+function writeFile(path, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(path, content, (err) => {
+            if (err) reject(err);
+            resolve();
+        });
+    });
+}
+
+module.exports = {
+    mkdir,
+    writeFile
+};
